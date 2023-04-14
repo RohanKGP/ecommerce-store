81abd5760f10751a85a8d236c5f41b846f2e4810
RohanKGP <meshramrohan786@gmail.com>
2023-04-14 15:02:01 +0530

    Fixed: Nodemailer templete

--- server/controllers/orders.js
+++ server/controllers/orders.js
@@ -1,6 +1,9 @@
 const nodemailer = require("nodemailer");
 
-async function sendEmail(user) {
+async function sendEmail(data) {
+  console.log(data.email);
+  console.log(data.list);
+
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
     service: "gmail",
@@ -10,13 +13,35 @@ async function sendEmail(user) {
     },
   });
 
+  let listString = "";
+  data.list.forEach(function (product) {
+    listString += `<li style="display: flex; border-bottom: 1px solid #ccc; padding: 10px;">
+    <div style="flex: 1;">
+      <img src="${product.image}" style="width:150px; height:150px;">
+    </div>
+    <div style="flex: 2; padding: 0 20px;">
+      <h3 style="color: #333; font-size: 18px;">${product.title}</h3>
+      <p style="font-size: 16px; color: #666;">${product.price}</p>
+    </div>
+  </li>`;
+  });
+
   // send mail with defined transport object
   let info = {
-    from: '"Rohan Raju" <meshramrohan786@gmail.com>',
-    to: "testwork0786@gmail.com", // list of receivers
-    subject: "Your Order:", // Subject line
-    html: "", // html body
+    from: '"Rohan Raju Meshram" <meshramrohan786@gmail.com>',
+    to: `${data.email}`, // list of receivers
+    subject: "Thanks for Ordering with us!", // Subject line
+    html: `<div style="background-color: #f2f2f2; padding: 20px;">
+    <h2 style="color: #333; text-align: center;">Thank you for your order!</h2>
+    <p style="font-size: 16px;">Here are the details of your order:</p>
+    <ul style="list-style: none; padding: 0;">
+      ${listString}
+    </ul>
+    <p style="font-size: 16px;">If you have any questions or concerns, please don't hesitate to contact us.</p>
+  </div>`, // html body
   };
+
+  console.log(`List String: ${listString}`);
 
   await transporter.sendMail(info, (err) => {
     if (err) {
@@ -26,14 +51,13 @@ async function sendEmail(user) {
     }
   });
 
-  console.log(`Message sent: ${JSON.stringify(info)}`);
+  // console.log(`Message sent: ${JSON.stringify(info)}`);
 }
 
 async function getOrders(req, res) {
-  const user = req.body;
-  console.log(user);
+  const data = req.body;
 
-  await sendEmail(user);
+  await sendEmail(data);
 
   return res.status(200).json({
     message: "Orders added successfully",
